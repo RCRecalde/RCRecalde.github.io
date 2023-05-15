0bffd6d8f5ad07abb45b83c12ca3f80cf23f54c4
RCRecalde <rorecalde3@gmail.com>
2023-05-15 20:57:23 -0300

    reloj con js

--- javascript/animacion.js
+++ javascript/animacion.js
@@ -2,3 +2,37 @@ document.addEventListener('mouseout', function tiping(event) {
     event.target.classList.add('tiping');
 });
     
+
+Number.prototype.num = function(n) {
+    //convierto el number a string
+    for (var r = this.toString(); r.length < n; r = 0 + r);
+    return r;
+  };
+  
+  function actualiza() {
+
+    var ahora = new Date();
+      min = ahora.getMinutes(),
+      hor = ahora.getHours(),
+      mes = ahora.getMonth(),
+      dia = ahora.getDate(),
+      año = ahora.getFullYear();
+
+    var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    var etiq = ["mes", "d", "a", "h", "m"],
+
+    //toma el valor actual que voy a recorrer
+    corr = [meses[mes], dia, año, hor.num(2), min.num(2)];
+
+    //nodeValue actualiza en tiempo real
+
+    for (var i = 0; i < etiq.length; i++)
+      document.getElementById(etiq[i]).firstChild.nodeValue = corr[i];
+
+  }
+  
+  function iniciar() {
+    actualiza();
+    window.setInterval("actualiza()", 1);
+  }
